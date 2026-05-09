6d75f1555998ea657c01e1e19fc0b3cc71624ddd
msjabata25 <msjabata25@cit.just.edu.jo>
2026-05-09 21:26:32 +0300

    tryna fix the cors issue

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://localhost:3000",
+        "https://msjabata25.github.io/pragma/"
     ],
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
